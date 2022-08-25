1f30a210005558ee1b534f3ecf24e9db0968bf62
Matthew Clarke <m_d_clarke@hotmail.com>
2022-08-25 14:05:27 +0200

    feat: added add booking button

--- app/components/Scheduler.client.tsx
+++ app/components/Scheduler.client.tsx
@@ -1,13 +1,40 @@
-import { useRef } from "react";
-import { BryntumScheduler } from "@bryntum/scheduler-react";
+import { useCallback, useRef } from "react";
+import { BryntumButton, BryntumScheduler } from "@bryntum/scheduler-react";
 import type { Scheduler } from "@bryntum/scheduler";
 import { schedulerConfig } from "~/schedulerConfig";
+import { Toast } from "@bryntum/scheduler";
 
 export default function SchedulerApp() {
   const schedulerRef = useRef<BryntumScheduler>(null);
   const schedulerInstance = () => schedulerRef.current?.instance as Scheduler;
+
+  const addEvent = useCallback(() => {
+    const scheduler = schedulerInstance();
+    const startDate = new Date(scheduler.startDate.getTime());
+    const endDate = new Date(startDate.getTime() + 2 * 86400000);
+    const resource = scheduler.resourceStore.first;
+
+    if (!resource) {
+      Toast.show("There is no resource available");
+      return;
+    }
+
+    endDate.setHours(endDate.getHours() + 2);
+
+    scheduler.eventStore.add({
+      resourceId: resource.id,
+      startDate: startDate,
+      endDate: endDate,
+      name: "New booking",
+      eventType: "Booking",
+    });
+  }, []);
+
   return (
     <>
+      <div className="demo-toolbar align-right">
+        <BryntumButton icon="b-fa-plus" cls="b-green" onClick={addEvent} />
+      </div>
       <BryntumScheduler ref={schedulerRef} {...schedulerConfig} />
     </>
   );
